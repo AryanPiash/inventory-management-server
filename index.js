b2a0e6c14802029b45bfb650a395a31cc7da09b2
AryanPiash <aryanpiash420@gmail.com>
2022-05-11 16:47:54 +0600

    change somethin for heroku

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
-// middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iytqp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 
 
 function verifyJWT(req, res, next) {
@@ -99,16 +98,14 @@ async function run() {
         app.post('/login', async (req, res) => {
             const user = req.body;
             const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-                expiresIn: '1d' 
+                expiresIn: '1d'
             });
             res.send({ accessToken });
         })
 
       
     }
-    finally {
-
-    }
+    finally {}
 }
 
 run().catch(console.dir);
